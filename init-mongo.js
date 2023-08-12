784a1a1485ad2ef5f2d29820498eef5ab1ce6f08
itterum <itterum@proton.me>
2023-08-12 19:17:08 +0300

    fix env for db

--- init-mongo.js
+++ init-mongo.js
@@ -11,6 +11,7 @@ db.createUser({
   user: process.env.MONGO_INITDB_ROOT_USERNAME,
   pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [
-    { role: "readWrite", db: dbName }
+    { role: "readWrite", db: dbName },
+    { role: "readWrite", db: "admin" },
   ]
 });
