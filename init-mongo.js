bdb995f82f085a10980efb82910f446994a12837
itterum <itterum@proton.me>
2023-08-12 19:19:45 +0300

    fix env for db

--- init-mongo.js
+++ init-mongo.js
@@ -6,12 +6,23 @@ var dbName = process.env.MONGO_DB_NAME || "mydb";
 // Создание базы данных
 db = db.getSiblingDB(dbName);
 
-// Создание пользователя
+// Создание пользователя в базе данных admin
+db.getSiblingDB("admin").createUser({
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  roles: [
+    { role: "root", db: "admin" },
+  ]
+});
+
+// Создание пользователя в базе данных mydb
 db.createUser({
   user: process.env.MONGO_INITDB_ROOT_USERNAME,
   pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [
     { role: "readWrite", db: dbName },
-    { role: "readWrite", db: "admin" },
   ]
 });
+
+console.log("Username:", process.env.MONGO_INITDB_ROOT_USERNAME);
+console.log("Password:", process.env.MONGO_INITDB_ROOT_PASSWORD);
